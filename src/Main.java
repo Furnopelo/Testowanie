84c2f89ac6fa0fcc016ec298e35e520d5d8bf9bf
Furno <furnoburno.fb@gmail.com>
2023-10-04 09:15:33 +0200

    v1.0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(12345);
+
+        int a = 12;
+        int b = 45;
+
+        System.out.println(a + b + " wynik");
+        System.out.println("wynik " + a + b);
+
+        System.out.println((double) b / a);
+    }
+}
