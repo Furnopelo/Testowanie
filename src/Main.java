8428280936d5612835a52f36d8c53195173c377b
Furno <furnoburno.fb@gmail.com>
2023-10-04 09:40:54 +0200

    v2.0

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(12345);
+        Scanner scanner = new Scanner(System.in);
 
-        int a = 12;
-        int b = 45;
+        System.out.println("Podaj pierwszą liczbę:");
+        double number1 = scanner.nextDouble();
 
-        System.out.println(a + b + " wynik");
-        System.out.println("wynik " + a + b);
+        System.out.println("Podaj drugą liczbę:");
+        double number2 = scanner.nextDouble();
 
-        System.out.println((double) b / a);
+        double result = number1 * number2;
+
+        System.out.println("Wynik mnożenia = " + result);
+
+        System.out.printf("Wynik mnożenia liczby %.3f i %.3f = %.2f \n", number1, number2, result);
     }
 }
